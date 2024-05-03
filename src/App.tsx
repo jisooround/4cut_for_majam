1f540deadc01c87a856374eae9aa178bcb8da046
jisooround <9utesleben@gmail.com>
2024-05-04 00:18:36 +0900

    design: 프레임 이미지 추가& 스타일수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [imgFile, setImgFile] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log("imgfile::::::::::::::: ", imgFile);
+    console.log("imgfile:::::::: ", imgFile);
   }, [imgFile]);
 
   const saveImgFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,29 +43,41 @@ function App() {
     }
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleImageClick = (index: any) => {
+    const newImgFile = [...imgFile];
+    newImgFile[index] = "/public/sohee.png"; // 클릭한 이미지의 인덱스를 "/public/sohee.png"로 대체
+    setImgFile(newImgFile);
+  };
+
   return (
     <>
-      <div className="w-screen h-screen bg-blue-600">
+      <div className="w-screen h-screen bg-yellow-100">
         <div className="text-center py-10">
           <h2 className="text-5xl">마천동 성당 어린이날</h2>
           <h4>인생네컷~!</h4>
         </div>
-        <div className="w-1/3 aspect-square bg-blue-600 m-auto relative flex justify-end">
-          <label className="cursor-pointer absolute bottom-[-100px] px-12 py-2 bg-blue-600 rounded-lg text-white border-solid border-2 border-white-600" htmlFor="image_one">
+        <div className="w-1/3 h-auto m-auto relative flex justify-center">
+          <label className="cursor-pointer fixed top-[800px] px-12 py-2 bg-yellow-100 rounded-lg text-blue-600 border-solid border-2 border-blue-600" htmlFor="image_one">
             📂 이미지 추가하기
           </label>
-          <label className="cursor-pointer absolute bottom-[-160px] right-[0] px-12 py-2 bg-blue-600 rounded-lg text-white border-solid border-2 border-white-600" htmlFor="image_one">
+          <label className="cursor-pointer fixed top-[860px] px-12 py-2 bg-yellow-100 rounded-lg text-blue-600 border-solid border-2 border-blue-600" htmlFor="image_one">
             📂 이미지 내보내기
           </label>
           <input type="file" onChange={(e) => saveImgFile(e)} id="image_one" multiple accept="image/*, .heic" className="hidden absolute" ref={imgRef} />
-          <div className="">
-            {imgFile.map((item, index) => {
-              return (
-                <div key={index} className="w-56 aspect-video bg-slate-50 overflow-hidden object-cover relative mb-3 pb-3 flex justify-center items-center">
-                  <img src={imgFile.length === 0 ? "/public/sohee.png" : item} width={"auto"} height={100} alt="img" className="z-90" />
-                </div>
-              );
-            })}
+          <div className="w-56 h-auto flex justify-center flex-wrap">
+            <div className="absolute z-10 left-0 right-0 mx-auto w-56">
+              <img src="/public/frame.png" alt="" />
+            </div>
+            <div className="pt-20">
+              {imgFile.map((item, index) => {
+                return (
+                  <div key={index} onClick={(index) => handleImageClick(index)} className="w-full aspect-video bg-slate-50 overflow-hidden object-cover relative flex justify-center items-center">
+                    <img src={imgFile.length === 0 ? "/public/sohee.png" : item} width={"auto"} height={100} alt="img" className="z-90" />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
