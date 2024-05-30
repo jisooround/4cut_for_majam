d4fec9e0ebc5a6b2d604674ff732bf021feb6998
jisooround <9utesleben@gmail.com>
2024-05-30 17:30:02 +0900

    feat:컨트롤러 >  닫기버튼 추가

--- src/components/Floating.tsx
+++ src/components/Floating.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Floating = () => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const onClickIcon = () => {
+    setShow(true);
+  };
+
+  const onClickClose = () => {
+    setShow(false);
+  };
   return (
-    <div className="fixed bottom-10 right-6">
-      <img className="animate-updown w-16 cursor-pointer" src="/setting.png" alt="" />
-    </div>
+    <>
+      {show ? (
+        <div className="fixed bottom-10 right-6 w-[300px] h-[500px] rounded-xl bg-white shadow-lg z-50">
+          <button onClick={onClickClose}>close</button>
+        </div>
+      ) : null}
+      <div onClick={onClickIcon} className="fixed bottom-10 right-6">
+        <img className="animate-updown w-16 cursor-pointer z-100" src="/setting.png" alt="" />
+      </div>
+    </>
   );
 };
 
